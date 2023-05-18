e5e4b51bd6159dbe73f8100a9574612dff8bf3d0
“Scablion” <Scablion>
2023-05-18 09:01:54 +0300

    add file №3!

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число A:");
+            int A = int.Parse(Console.ReadLine());
+            for(int i = 2; i <= 8; i *= 2)
+            {
+                Console.WriteLine($"А в {i} степени = {Pow(A,i)}");
+            }
+        }
+    }
+}
