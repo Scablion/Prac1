44fcce7466b77ada84cceaf0fcd7ecfbc24ab1ee
Scablion <kashcada24@gmail.com>
2023-05-20 08:50:11 +0300

    Now all project

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            int a = int.Parse(Console.ReadLine());
+            int x = a / 12;
+            double b = Exp(-a * x);
+            double y = 1 - Exp(-a * x) * Sin(a * x + b);
+            Console.WriteLine($"y = {y:f3}");
+            Console.ReadKey();
+        }
+    }
+}
